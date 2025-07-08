e9fd05b52347ffd074c8158e9c455db047c336f3
Dr.T. <Aohudyakov@gmail.com>
2025-07-09 02:55:31 +0500

    немного подкорректировал

--- home_work/catalog/views.py
+++ home_work/catalog/views.py
@@ -29,27 +29,3 @@ def contacts(request):
     # Получаем контактные данные из базы
     contact_info = Contact.objects.first()
     return render(request, "contacts.html", {"contact_info": contact_info})
-
-
-# from django.shortcuts import render
-# from django.http import HttpResponse
-#
-#
-# # Create your views here.
-#
-#
-# def home(request):
-#     return render(request, "home.html")
-#
-#
-# def contacts(request):
-#     if request.method == "POST":
-#         name = request.POST.get("name")
-#         phone = request.POST.get("phone")
-#         message = request.POST.get("message")
-#         return HttpResponse(
-#             f"""
-#             Спасибо, {name}. Ваше сообщение успешно отправлено!
-#     Мы свяжемся с Вами по номеру телефона {phone}"""
-#         )
-#     return render(request, "contacts.html")
