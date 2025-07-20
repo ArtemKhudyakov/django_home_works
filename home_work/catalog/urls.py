e75661da6dd19b4679ce8d75c9ee8d2e1e57e5b5
Dr.T. <Aohudyakov@gmail.com>
2025-07-21 01:08:13 +0500

    переделал контроллеры на классы

--- home_work/catalog/urls.py
+++ home_work/catalog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import home, contacts, greeting, product_details
+from .views import HomeView, ContactsView, GreetingView, ProductDetailView
 
 app_name = "catalog"
 
 urlpatterns = [
-    path("", greeting, name="greeting"),
-    path("home/", home, name="home"),
-    path("contacts/", contacts, name="contacts"),
-    path("product_details/<int:pk>/", product_details, name="product_details"),
+    path("", GreetingView.as_view(), name="greeting"),
+    path("home/", HomeView.as_view(), name="home"),
+    path("contacts/", ContactsView.as_view(), name="contacts"),
+    path("product_details/<int:pk>/", ProductDetailView.as_view(), name="product_details"),
 ]
--- home_work/catalog/views.py
+++ home_work/catalog/views.py
@@ -1,42 +1,83 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView, TemplateView, DetailView, FormView
+from django.urls import reverse_lazy
+
 from .models import Product, Contact
 
 
-def greeting(request):
-    return render(request, 'greeting.html')
+# def greeting(request):
+#     return render(request, 'greeting.html')
+
+class GreetingView(TemplateView):
+    template_name = 'greeting.html'
 
 
-def home(request):
-    # Получаем последние 5 созданных продуктов
-    latest_products = Product.objects.order_by("-created_at")[:5]
+# def home(request):
+#     # Получаем последние 5 созданных продуктов
+#     latest_products = Product.objects.order_by("-created_at")[:5]
+#
+#     context = {'latest_products': latest_products}
+#     # Выводим в консоль
+#     print("Последние 5 добавленных продуктов:")
+#     for product in latest_products:
+#         print(f"{product.name} - {product.created_at}")
+#
+#     return render(request, "home.html", context)
 
-    context = {'latest_products': latest_products}
-    # Выводим в консоль
-    print("Последние 5 добавленных продуктов:")
-    for product in latest_products:
-        print(f"{product.name} - {product.created_at}")
 
-    return render(request, "home.html", context)
+class HomeView(ListView):
+    model = Product
+    template_name = 'home.html'
+    context_object_name = 'latest_products'
 
+    def get_queryset(self):
+        # Получаем последние 5 созданных продуктов
+        latest_products = super().get_queryset().order_by("-created_at")[:5]
+        return latest_products
 
-def contacts(request):
-    if request.method == "POST":
+# def contacts(request):
+#     if request.method == "POST":
+#         name = request.POST.get("name")
+#         phone = request.POST.get("phone")
+#         message = request.POST.get("message")
+#         return HttpResponse(
+#             f"""
+#             Спасибо, {name}. Ваше сообщение успешно отправлено!
+#     Мы свяжемся с Вами по номеру телефона {phone}"""
+#         )
+#
+#     # Получаем контактные данные из базы
+#     contact_info = Contact.objects.first()
+#     return render(request, "contacts.html", {"contact_info": contact_info})
+
+class ContactsView(TemplateView):
+    template_name = "contacts.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['contact_info'] = Contact.objects.first()
+        return context
+
+    def post(self, request):
         name = request.POST.get("name")
         phone = request.POST.get("phone")
         message = request.POST.get("message")
         return HttpResponse(
             f"""
             Спасибо, {name}. Ваше сообщение успешно отправлено!
-    Мы свяжемся с Вами по номеру телефона {phone}"""
+            Мы свяжемся с Вами по номеру телефона {phone}"""
         )
 
-    # Получаем контактные данные из базы
-    contact_info = Contact.objects.first()
-    return render(request, "contacts.html", {"contact_info": contact_info})
+# def product_details(request, pk):
+#     product = Product.objects.get(pk=pk)
+#     context = {"product": product}
+#     return render(request, "product_details.html", context)
 
-def product_details(request, pk):
-    product = Product.objects.get(pk=pk)
-    context = {"product": product}
-    return render(request, "product_details.html", context)
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'product_details.html'
+
+
+
+
 
