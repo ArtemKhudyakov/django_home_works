7b08cdae5503d2631712cc976a5cff6e6b75bfb0
Dr.T. <Aohudyakov@gmail.com>
2025-07-26 03:18:26 +0500

    сверстал страницы приветсвия, списка статей настроил связи между страницами

--- home_work/blog/urls.py
+++ home_work/blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import GreetingView
+from .views import GreetingView, ArticleListView
 
 app_name = "blog"
 
 urlpatterns = [
     path("", GreetingView.as_view(), name="greeting"),
+    path("articles/", ArticleListView.as_view(), name="article_list"),
     # path("home/", HomeView.as_view(), name="home"),
     # path("contacts/", ContactsView.as_view(), name="contacts"),
     # path("product_details/<int:pk>/", ProductDetailView.as_view(), name="product_details"),
--- home_work/blog/views.py
+++ home_work/blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import ListView, TemplateView, DetailView, FormView
 from django.urls import reverse_lazy
+from django.db.models import Count
 
 from .models import Article
 
@@ -21,4 +22,26 @@ class GreetingView(TemplateView):
         context['latest_articles'] = Article.objects.filter(
             publication_status=True
         ).order_by('-created_at')[:5]
+        return context
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'article_list.html'
+    context_object_name = 'articles'
+    paginate_by = 10
+
+    def get_queryset(self):
+
+        return Article.objects.filter(
+            publication_status=True
+        ).order_by('-created_at')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Получаем топ-3 авторов по количеству статей
+        context['top_authors'] = Article.objects.values(
+            'author'
+        ).annotate(
+            article_count=Count('id')
+        ).order_by('-article_count')[:3]
         return context
