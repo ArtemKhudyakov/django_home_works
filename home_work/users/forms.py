023d6ed3051fadd065c0ae89f99fa30a9df7b1b1
Dr.T. <Aohudyakov@gmail.com>
2025-08-01 01:07:31 +0500

    начал ваять страницы логина логаута и регистрации

--- home_work/users/forms.py
+++ home_work/users/forms.py
@@ -1,4 +1,23 @@
 from django.core.exceptions import ValidationError
-from django.forms import ModelForm
+from django.forms import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import User
+
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("Пользователь с таким email уже существует.")
+        return email
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.filter(username=username).exists():
+            raise ValidationError("Пользователь с таким именем уже существует")
+        return username
+
+    class Meta:
+        model = User
+        fields = ('username', 'email','country', 'phone', 'avatar', 'password1', 'password2')
